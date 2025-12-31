a749081e6f5f60a0b0bb4cf6f6a649caf9064b44
JamesParkerStrand <78117256+JamesParkerStrand@users.noreply.github.com>
2025-12-31 03:59:38 -0800

    Update npcGoalStates.js

--- src/npcGoalStates.js
+++ src/npcGoalStates.js
@@ -2,7 +2,7 @@
 
 export var npcTextIndexToGameInstructions = new Map();
 
-// MeshesToChange: [(MeshIdToChange, newLoadedMeshname)...]
+// MeshesToChange: [(noaEntityIdToChange, newLoadedMeshname)...]
 npcTextIndexToGameInstructions.set(3, {MeshesToChange: [[3, 'teleportActive']]});
 
 // npcTextIndex: storage of win conditions to move on to the next text
